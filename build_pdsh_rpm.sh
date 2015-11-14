02090d03ee655142fc61ece1867c926da9a6a4bd
ruilinchu <ruilinchu@gmail.com>
2015-11-13 21:01:04 -0800

    update

--- build_pdsh_rpm.sh
+++ build_pdsh_rpm.sh
@@ -2,5 +2,6 @@
 version=2.29
 wget https://pdsh.googlecode.com/files/pdsh-$version.tar.bz2
 yum install -y gcc rpm-build readline-devel ncurses-devel pam-devel
-rpmbuild -ta pdsh-$version.tar.bz2
-yum install -y ~/rpmbuild/RPMS/x86_64/pdsh-*.rpm
+## error building with root
+runuser -l vagrant -c "rpmbuild -ta /vagrant/pdsh-$version.tar.bz2"
+yum install -y /home/vagrant/rpmbuild/RPMS/x86_64/pdsh-*.rpm
