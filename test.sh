b37f8204dd30f501daead7fe3884704262ba1ce0
ruilinchu <ruilinchu@gmail.com>
2015-11-13 20:12:31 -0800

    add

--- test.sh
+++ test.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
-ceph osd pool create cephfs_data 256
-sleep 100
-ceph osd pool create cephfs_metadata 256
+# correct the pg and pgp num first ceph always set 64
+ceph osd pool set rbd pg_num `grep pg_num edit_ceph-ansible-master.sh | awk '{print $2}'`
+sleep 60
+ceph osd pool set rbd pgp_num `grep pgp_num edit_ceph-ansible-master.sh | awk '{print $2}'`
 sleep 30
+
+ceph osd pool create cephfs_data `grep pg_num edit_ceph-ansible-master.sh | awk '{print $2}'`
+ceph osd pool create cephfs_metadata `grep pg_num edit_ceph-ansible-master.sh | awk '{print $2}'`
 ceph fs new cephfs cephfs_metadata cephfs_data
 ceph fs ls
 ceph -s
