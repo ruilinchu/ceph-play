61e3b071412faafc81cdd4d9ad974a7133e7a4fe
ruilinchu <ruilinchu@gmail.com>
2015-11-15 10:25:08 -0800

    update

---
+++ rebuild_box.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+yum -y update
+rpm -Uhv http://ceph.com/rpm-hammer/el7/noarch/ceph-release-1-1.el7.noarch.rpm
+yum -y install epel-release
+yum -y install ntp snappy leveldb gdisk python-argparse gperftools-libs attr
+yum -y install ceph ceph-deploy --disablerepo=epel
+
+yum clean all
+dd if=/dev/zero of=/EMPTY bs=1M
+rm -f /EMPTY
+
+cat /dev/null > ~/.bash_history && history -c && exit
+
+# vagrant package --output mynew.box
+# vagrant box add mynewbox mynew.box
