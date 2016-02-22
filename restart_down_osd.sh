01254319467de215577aef32a738f1828e378fbd
ruilinchu <ruilinchu@gmail.com>
2016-02-22 11:48:43 -0800

    update

--- restart_down_osd.sh
+++ restart_down_osd.sh
@@ -3,5 +3,5 @@
 for down_osd in $(ceph osd tree | awk '/down/ {print $1}')
 do
     host=$(ceph osd find $down_osd | awk -F\" '$2 ~ /host/ {print $4}')
-    ssh $host restart ceph-osd id=$down_osd
+    ssh $host systemctl restart ceph-osd@$down_osd
 done
